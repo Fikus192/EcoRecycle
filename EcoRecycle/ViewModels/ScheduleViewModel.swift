9b4f7ebb744cc00bcb71ae6dc4adc4856bf6ee9c
Mateusz Ratajczak <80542171+Fikus192@users.noreply.github.com>
2024-01-08 19:41:26 +0100

    ER-43 ScheduleView modificated, SwiftData library added (#20)

--- EcoRecycle/ViewModels/ScheduleViewModel.swift
+++ EcoRecycle/ViewModels/ScheduleViewModel.swift
@@ -11,9 +11,30 @@ import SwiftUI
 final class ScheduleViewModel: ObservableObject {
     
     @Published private(set) var schedules: [Schedule]
+    @Published var showInfo: Bool = false
+    @Published var showAddLocation: Bool = false
+    @Published var selectedLocation: Schedule?
     
     init() {
         let schedules = Schedule.allSchedules
         self.schedules = schedules
+    }
+    
+    @ViewBuilder
+    internal func scheduleDetailsView(for schedule: Schedule) -> some View {
+        ForEach(schedule.pickup.allWasteTypes, id: \.self) { wasteType in
+            Section(schedule.pickup.convertIntoPolish(for: wasteType)) {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(schedule.pickup.dates(for: wasteType), id: \.self) { date in
+                            ScheduleRow(
+                                color: schedule.pickup.color(forWasteType: wasteType),
+                                dates: [date])
+                        }
+                    }
+                }
+                .listRowSeparator(.hidden, edges: .bottom)
+            }
+        }
     }
 }
---
+++ EcoRecycle/Views/Schedules/ScheduleInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleInfoView.swift
+//  EcoRecycle
+//
+//  Created by Mateusz Ratajczak on 04/01/2024.
+//
+
+import SwiftUI
+
+struct ScheduleInfoView: View {
+    var body: some View {
+        Text("Info View")
+    }
+}
+
+#Preview {
+    ScheduleInfoView()
+}
