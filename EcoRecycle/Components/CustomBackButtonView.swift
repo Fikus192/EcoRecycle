4215dc7dedb0ff74a9420bc8bdaca51674f2fd6b
Mateusz Ratajczak <80542171+Fikus192@users.noreply.github.com>
2024-01-10 18:52:36 +0100

    ER-44 - ScheduleLocationView add (#22)

---
+++ EcoRecycle/Components/CustomBackButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  CustomBackButtonView.swift
+//  EcoRecycle
+//
+//  Created by Mateusz Ratajczak on 10/01/2024.
+//
+
+import SwiftUI
+
+struct CustomBackButtonView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image(systemName: "arrowshape.turn.up.backward")
+        }
+    }
+}
+
+#Preview {
+    CustomBackButtonView()
+}
